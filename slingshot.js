0fc64fd96106c97fbd2fc1a7e995b5cc016ab47d
SHREE127 <71012678+SHREE127@users.noreply.github.com>
2020-10-27 17:26:10 +0530

    Update slingshot.js

--- slingshot.js
+++ slingshot.js
@@ -1,7 +1,5 @@
-class Sling {
-
+/*class Sling {
     constructor(bodyA,pointB){
-
         var options={
 
             bodyA: bodyA,
@@ -9,26 +7,45 @@ class Sling {
             stiffness: 0.04,
             lenght: 2
         }
-    
         this.object = Constraint.create(options);
- 
 	    World.add(world,this.object);
         this.pointB = pointB
     }
-
 display(){
-
     var a = this.object.bodyA.position;
     var b = this.pointB;
-
     line(a.x,a.y,b.x,b.y)
 }
-
 fly(){
-
 	this.object.bodyA=null;
+}
+}*/
+
+class Sling {
+    constructor(bodyA,pointB){
+        var options={
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            lenght: 2
+        }
+        this.object = Constraint.create(options);
+	    World.add(world,this.object);
+        this.pointB = pointB
+    }
+display(){
+ if (this.object.bodyA){
+     push();
+var a = this.object.bodyA.position;
+var b = this.pointB;
+	 strokeWeight(10);
+    line(a.x-20,a.y,b.x-10,b.y);
+    //line(a.x-20,a.y,b.x+30,b.y); 
+    pop();
 
 }
-
-
+}
+fly(){
+	this.object.bodyA=null;
+}
 }
